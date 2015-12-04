2063694baefb7499a2b959679eba26d306ebefe9
jasonmaier <j_maier@berkeley.edu>
2015-12-03 22:39:04 -0800

    commit7

--- config.py
+++ config.py
@@ -12,16 +12,23 @@ OPENID_PROVIDERS = [
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = ('sqlite:///' + os.path.join(basedir, 'app.db') +
+                               '?check_same_thread=False')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_RECORD_QUERIES = True
 WHOOSH_BASE = os.path.join(basedir, 'search.db')
+
+# Whoosh does not work on Heroku
+WHOOSH_ENABLED = os.environ.get('HEROKU') is None
 
 # slow database query threshold (in seconds)
 DATABASE_QUERY_TIMEOUT = 0.5
 
 # email server
-MAIL_SERVER = 'your.mailserver.com'
+MAIL_SERVER = ''  # your mailserver
 MAIL_PORT = 25
 MAIL_USE_TLS = False
 MAIL_USE_SSL = False
@@ -35,8 +42,8 @@ LANGUAGES = {
 }
 
 # microsoft translation service
-MS_TRANSLATOR_CLIENT_ID = '' # enter your MS translator app id here
-MS_TRANSLATOR_CLIENT_SECRET = '' # enter your MS translator app secret here
+MS_TRANSLATOR_CLIENT_ID = ''  # enter your MS translator app id here
+MS_TRANSLATOR_CLIENT_SECRET = ''  # enter your MS translator app secret here
 
 # administrator list
 ADMINS = ['you@example.com']
@@ -44,6 +51,3 @@ ADMINS = ['you@example.com']
 # pagination
 POSTS_PER_PAGE = 50
 MAX_SEARCH_RESULTS = 50
-
-# Whoosh does not work on Heroku
-WHOOSH_ENABLED = os.environ.get('HEROKU') is None
