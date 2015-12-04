52e2d0ae2b19e9e2771edf966bd1ee12af91b50f
jasonmaier <j_maier@berkeley.edu>
2015-12-04 11:20:56 -0800

    cm1

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+from flask import Flask, render_template
+import sys
+import logging
+app = Flask(__name__)
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
+
 #!flask/bin/python
 from app import app
 app.run(debug = True)
